5abdd6c43b04c487001e3209e8bfa7662ef95580
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-02-26 18:01:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inlämningsuppgifterkapitel6._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(plusa(3, 8, 9));
+        }
+
+        static int plusa(int tal1, int tal2, int tal3)
+        {
+            int sum = tal1 + tal2 + tal3;
+            return sum;
+        }
+
+    }
+}
